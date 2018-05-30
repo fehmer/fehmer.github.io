7501670c3e5c74f6097b8a52fd9ce218858885fe
Christian Fehmer <cfe@sexy-developer.com>
2018-05-30 22:29:40 +0200

    fresh new start

--- TODO.md
+++
@@ -1,6 +0,0 @@
-#blog
-- why i use eclipse, sublime, brunch, git etc
-- tdd, broken window, kiss, dry etc
-- rest for java lovers
-- 
-
--- src/files/scripts/scrollspy.js
+++
@@ -1,8 +0,0 @@
-/* ========================================================================
- * Bootstrap: scrollspy.js v3.2.0
- * http://getbootstrap.com/javascript/#scrollspy
- * ========================================================================
- * Copyright 2011-2014 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
-
